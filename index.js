3d797640042b03f42929caad3965135a8d248ffa
Ketema Girma <ketemagirma15@gmail.com>
2022-03-22 15:26:26 +0300

    update

--- index.js
+++ index.js
@@ -38,49 +38,4 @@ fetch("./data.json")
                 </div>
                 `
         }
-
     })
-
-// function age(birthYear) {
-//     let calculatedAge = new Date().getFullYear() - birthYear;
-//     if (calculatedAge == 1) {
-//         return "1 year old";
-//     } else if (calculatedAge == 0) {
-//         return "Baby";
-//     } else {
-//         return `
-//             $ { calculatedAge }
-//             years old `;
-//     }
-// }
-
-// function foods(foods) {
-//     return ` <h4 > Favorite Foods < /h4> 
-//<ul class = "foods-list"> $ { foods.map(food => ` < li > $ { food } < /li>`).join("") } </ul >
-//         `;
-//       }
-
-// function petTemplate(pet) {
-//     return ` <
-//         div class = "animal" >
-//         <
-//         img class = "pet-photo"
-//     src = "${pet.photo}" >
-//         <
-//         h2 class = "pet-name" > $ { pet.name } < span class = "species" > ($ { pet.species }) < /span></h
-//     2 >
-//         <
-//         p > < strong > Age: < /strong> ${age(pet.birthYear)}</p >
-//         $ { pet.favFoods ? foods(pet.favFoods) : "" } <
-//         /div>
-//     `;
-//       }
-
-//       document.getElementById("app").innerHTML = ` <
-//     h1 class = "app-title" > Pets($ { petsData.length }
-//         results) < /h1>
-//     $ { petsData.map(petTemplate).join("") } <
-//     p class = "footer" > These $ { petsData.length }
-//     pets were added recently.Check back soon
-//     for updates. < /p>
-//     `;
